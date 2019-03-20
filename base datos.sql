7944d6dc1c11ba78992f04f198b2ebb85a589f18
MarinaLara <34198991+MarinaLara@users.noreply.github.com>
2019-03-19 17:34:25 -0700

    #2
    Base de datos actual 19/03/19

--- base datos.sql
+++ base datos.sql
@@ -1,7 +1,16 @@
 
 CREATE DATABASE SLYL;
+use SLYL;
 
 
+DROP TABLE IF EXISTS `cat_niveles`;
+
+CREATE TABLE `cat_niveles`(
+	`id_nivel` int (11) not null auto_increment,
+    `departamento` varchar(30),
+    `nivel_usuario` int,
+    PRIMARY KEY (`id_nivel`)
+);
 -- ----------------------------
 -- Table structure for usuarios
 -- ----------------------------
@@ -15,13 +24,21 @@ CREATE TABLE `usuarios` (
   `id_nivel` int(11) DEFAULT NULL,
   `contrasena` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `activo` char(1) COLLATE utf8_unicode_ci DEFAULT '1',
-  `id_empresa` int(8) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`),
-  KEY `idEmpresa` (`id_nivel`)
+  FOREIGN KEY (`id_nivel`) REFERENCES `cat_niveles` (`id_nivel`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+INSERT INTO `cat_niveles` (departamento, nivel_usuario) VALUES ('ROOT', '0');
+INSERT INTO `cat_niveles` (departamento, nivel_usuario) VALUES ('ADMINISTRADOR', '1');
+INSERT INTO `cat_niveles` (departamento, nivel_usuario) VALUES ('CONTABILIDAD', '2');
+INSERT INTO `cat_niveles` (departamento, nivel_usuario) VALUES ('VENTAS', '3');
+INSERT INTO `cat_niveles` (departamento, nivel_usuario) VALUES ('VENTAS', '4');
+INSERT INTO `cat_niveles` (departamento, nivel_usuario) VALUES ('PRODUCCIÓN', '5');
 
 -- ----------------------------
 -- Records of usuarios
 -- ----------------------------
-INSERT INTO `usuarios` VALUES ('1', 'root@pinguinosystems.com', 'MARTIN FRANCISCO', 'MARTINEZ', 'FEDERICO', '1', '123456', '1', '1');
+INSERT INTO `usuarios` VALUES ('1', 'root@pinguinosystems.com', 'MARTIN FRANCISCO', 'MARTINEZ', 'FEDERICO', '1', '123456', '1');
 
